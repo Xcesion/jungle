63ac0117346390ce7917474cd7e72e5e8e4fe688
Frank Shen <frank851207@gmail.com>
2023-08-02 16:02:02 -0700

    cypress testing with product details

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,16 @@
+// it("There is 2 products on the page", () => {
+//   cy.get(".products article").should("have.length", 2);
+// });
+describe ('Jungle Home Page', ()=>{
+  beforeEach(() => {
+    cy.visit("http://0.0.0.0:3000")
+  })
+  
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+})
