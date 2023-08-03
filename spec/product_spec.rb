b0167484786a93bc2c97cd2cae889332b0489105
Frank Shen <frank851207@gmail.com>
2023-08-03 00:36:32 -0700

    user authentication testing with rspec completed

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
-    it 'contains validation for name field' do
+    it 'when the name field is nil' do
       @category = Category.new(name: 'test')
       @product = Product.new(name: nil, price_cents: 1000, quantity: 1, category: @category)
       expect(@product).to_not be_valid
     end
-    it 'contains validation for price field' do
+    it 'when the price is nil in the field' do
       @category = Category.new(name: 'test')
       @product = Product.new(name: 'john', price_cents: nil, quantity: 1, category: @category)
       expect(@product).to_not be_valid
     end
-    it 'contains validation for quantity field' do
+    it 'when the quantity field contains nil' do
       @category = Category.new(name: 'test')
       @product = Product.new(name: 'john', price_cents: 1000, quantity: nil, category: @category)
       expect(@product).to_not be_valid
     end
-    it 'contains validation for category field' do
+    it 'when the category field contains nil' do
       @category = Category.new(name: 'test')
       @product = Product.new(name: 'john', price_cents: 1000, quantity: 1, category: nil)
       expect(@product).to_not be_valid
